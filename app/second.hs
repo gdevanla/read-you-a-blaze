c4b406edd63931ce003f89691a61aee4ff81bf0e
grdvnl <grdvnl@gmail.com>
2020-08-05 08:43:36 -0700

    Update to align with changes to first.hs

--- app/second.hs
+++ app/second.hs
@@ -1,114 +1,178 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 {-# LANGUAGE GADTs #-}
 
---module Main1 where
+import           Prelude                 hiding ( head
+                                                , id
+                                                , div
+                                                )
 
 data Attribute = Attribute String String
   deriving (Show)
 
-data HtmlM a =
-  Html (HtmlM a)
+-- Note change in type from first.hs
+data HtmlM a = Html (HtmlM a)
   | Head (HtmlM a)
+  | Body (HtmlM a)
   | Link a
+  | Title String a
   | HRef String a
-  | Table (HtmlM a)
-  | Td (HtmlM a)
-  | Tr (HtmlM a)
-  | Th (HtmlM a)
-  | forall b. Append (HtmlM b) (HtmlM a) -- Note the new value `b`
---  | Parent (HtmlM a) (HtmlM a)
---  | Leaf (HtmlM a)
+  | Div (HtmlM a)
+  | UL (HtmlM a)
+  | LI (HtmlM a)
+  | P (HtmlM a)
+  | forall b. Append (HtmlM b) (HtmlM a)
   | AddAttribute Attribute (HtmlM a)
   | Content String a
   | Empty a
 
-
-htmlValue :: HtmlM a -> a
-htmlValue (Html x) =  htmlValue x
-htmlValue (Head x) =  htmlValue x
-htmlValue (Link x) = x
-htmlValue (HRef _ x) = x
-htmlValue (Table x) = htmlValue x
-htmlValue (Td x) = htmlValue x
-htmlValue (Tr x) = htmlValue x
-htmlValue (Th x) = htmlValue x
-htmlValue (Append _ x) = htmlValue x
-htmlValue (AddAttribute _ x) = htmlValue x
-htmlValue (Content _ x) = x
-htmlValue (Empty x) = x
-
-showValue :: (Show a) => HtmlM a -> String
-showValue (Html x) =  "(Html " ++ showValue x ++ ")"
-showValue (Head x) =  "(Head " ++ showValue x ++ ")"
-showValue (Link x) = "(Link " ++ show x ++ ")"
-showValue (HRef _ x) = "HRef " ++ show x ++ ")"
-showValue (Table x) = "(Table " ++ show (showValue x) ++ ")"
-showValue (Td x) = "(Td " ++ (showValue x) ++ ")"
-showValue (Tr x) = "(Tr " ++ (showValue x) ++ ")"
-showValue (Th x) = "(Th " ++ (showValue x) ++ ")"
-showValue (Append x y) = "(Append " ++ "_  " ++ (showValue y) ++ ")"
-showValue (AddAttribute attr x) = "(AddAttribute " ++ "(Attribute " ++ (show attr) ++ ")" ++ (showValue x) ++ ")"
-showValue (Content _ x) = "(Content " ++ show x ++ ")"
-showValue (Empty x) = "(Empty " ++ show x ++ ")"
-
 type Html = HtmlM ()
+type Markup = HtmlM ()
+
+-- Walk through the Html data structure to return the embedded value. Used in Functor, Applicative and Monad instances
+htmlValue :: HtmlM a -> a
+htmlValue (Html x          ) = htmlValue x
+htmlValue (Head x          ) = htmlValue x
+htmlValue (Link x          ) = x
+htmlValue (HRef _ x) = x
+htmlValue (Div x) = htmlValue x
+htmlValue (LI x) = htmlValue x
+htmlValue (UL x) = htmlValue x
+htmlValue (P x) = htmlValue x
+htmlValue (Append       _ x) = htmlValue x
+htmlValue (AddAttribute _ x) = htmlValue x
+htmlValue (Content      _ x) = x
+htmlValue (Empty x         ) = x
+
+--- Same as the version in first.hs
+renderMarkup :: String -> HtmlM a -> String
+renderMarkup attrs (Html inner) =
+        "<html" ++ attrs ++ ">" ++ renderMarkup "" inner ++ "</html>"
+renderMarkup attrs (Head inner) =
+        "<head" ++ attrs ++ ">" ++ renderMarkup "" inner ++ "</head>"
+renderMarkup attrs (Body inner) =
+        "<body" ++ attrs ++ ">" ++ renderMarkup "" inner ++ "</body>"
+renderMarkup attrs (P inner) =
+        "<p" ++ attrs ++ ">" ++ renderMarkup "" inner ++ "</p>"
+renderMarkup attrs (Link _) = "<link" ++ attrs ++ ">"
+renderMarkup attrs (Title s _ ) = "<title" ++ attrs ++ ">" ++ s ++ "</title>"
+renderMarkup attrs (HRef content _) = "<a" ++ attrs ++ ">" ++ content ++ "</a>"
+renderMarkup attrs (Div content) =
+        "<div" ++ attrs ++ ">" ++ (renderMarkup "" content) ++ "</div>"
+renderMarkup attrs (UL content) =
+        "<ul" ++ attrs ++ ">" ++ (renderMarkup "" content) ++ "</ul>"
+renderMarkup attrs (LI content) =
+        "<li" ++ attrs ++ ">" ++ (renderMarkup "" content) ++ "</li>"
+renderMarkup attrs (Append html1 html2) =
+  (renderMarkup attrs html1) ++ (renderMarkup attrs html2)
+renderMarkup attrs (AddAttribute (Attribute k v) html) = flip renderMarkup html $
+  " " ++ k ++ "\"" ++ v ++ "\""
+renderMarkup attrs (Content content _) = content
+renderMarkup attrs (Empty _) = ""
+
+href :: String -> Attribute
+href value = Attribute "href" value
+
+type_ :: String -> Attribute
+type_ value = Attribute "type" value
+
+-- -- Attribute related functions
+id :: String -> Attribute
+id value = Attribute "id" value
+
+rel :: String -> Attribute
+rel value = Attribute "rel=" value
+
+toMarkup :: String -> Html
+toMarkup s = Content s ()
 
 -- Need to update type of Append with existential
 instance Functor HtmlM where
-  fmap f x = Append x (Empty (f (htmlValue x)))
+        fmap f x = Append x (Empty (f (htmlValue x)))
 
 -- Why isn't this just an `(Empty (htmlValue x (htmlValue y)))`
 -- The `homomorphism` law is not followed here.
 instance Applicative HtmlM where
-  pure x = Empty x
+        pure x = Empty x
 
-  -- Why are we not discarding the first value?
-  (*>) = Append
-  (<*>) x y = --Empty (htmlValue x (htmlValue y))
-    Append (Append x y) (Empty (htmlValue x (htmlValue y)))
+        -- Why are we not discarding the first value?
+        (*>) = Append
+        (<*>) x y = --Empty (htmlValue x (htmlValue y))
+                Append (Append x y) (Empty (htmlValue x (htmlValue y)))
 
 instance Monad HtmlM where
-  return = pure
+        return = pure
 
-  m >>= f = Append m (f (htmlValue m))
-  (>>) = Append
+        m >>= f = Append m (f (htmlValue m))
+        (>>) = Append
 
 
--- Update to HtmlM a to accomodate change to type of Append
-renderString :: String -> HtmlM a -> String
-renderString attrs (Html inner) = "<html " ++ attrs ++ ">" ++ renderString "" inner ++ "</html>"
-renderString attrs (Head inner) = "<head " ++ attrs ++ ">" ++ renderString "" inner ++ "</head>"
-renderString attrs (Link _) = "<link "  ++ attrs ++ ">"
-renderString attrs (HRef content _) = "<a  " ++ attrs ++ ">" ++ content ++ "</a>"
-renderString attrs (Table content) = "<table  " ++ attrs ++ ">" ++ (renderString "" content) ++ "</table>"
-renderString attrs (Tr content) = "<tr  " ++ attrs ++ ">" ++ renderString "" content ++ "</tr>"
-renderString attrs (Th content) = "<th  " ++ attrs ++ ">" ++ renderString "" content ++ "</th>"
-renderString attrs (Td content) = "<td  " ++ attrs ++ ">" ++ renderString "" content ++ "</td>"
-renderString attrs (Append html1 html2) =
-  (renderString attrs html1) ++ (renderString attrs html2)
-renderString attrs (AddAttribute (Attribute k v) html) = flip renderString html $
-  k ++ "\"" ++ v ++ "\""
-renderString attrs (Content content _) = content
-renderString attrs (Empty _) = ""
+-- HTML helpders
+-- link :: Html
+-- link = Link
 
-href value = Attribute "href" value
-type_ value = Attribute "type" value
+-- title :: String -> Html
+-- title s = Title s
 
-rel value = Attribute "rel=" value
+-- head :: Html -> Html
+-- head = HTML
 
+-- div :: Html -> Html
+-- div = Div
+
+-- p :: Html -> Html
+-- p = P
+
+-- ul :: Html -> Html
+-- ul = UL
+
+-- li :: Html -> Html
+-- li = LI
+
+-- body = Html -> Html
+-- body = Body
+
+-- html = Html -> Html
+-- html = HTML
+
+-- We need to support the (!) operator as (!) and another version of it as `mult`'
 (!) :: Html -> Attribute -> Html
 (!) html attr = AddAttribute attr html
 
---linkElement = Link (Empty ())
+mult :: (Html -> Html) -> Attribute -> Html -> Html
+mult html1 attr = \html2 -> AddAttribute attr (html1 html2)
 
-myHtml :: Html
-myHtml = Html $ do
+foldHtml :: [Html] -> Html
+foldHtml (x:xs) = Append x (foldHtml xs)
+foldHtml [] = Empty ()
+
+-- page1 :: Html
+-- page1 = let
+--   link' = Link ! rel "style.css" ! type_ "text/html"
+--   title' = Title "Introduction page."
+--   head' = Head (Append link' title')
+--   div' = mult Div (id "header") $ (Content "Syntax")
+--   p' = P $ Content "This is an example of Blazemarkup syntax."
+--   ul' = UL $ foldHtml $ (map (LI . toMarkup . show) [1, 2, 3])
+--   body' = Body (Append (Append div' p') ul')
+--   html' = HTML $ Append head' body'
+--   in
+--     html'
+
+page1 :: Html
+page1 = Html $ do
   Head $ do
     Link () ! rel "style.css" ! type_ "text/html"
-    HRef "myLink" () ! href "http://read-you-a-blaze/"
+    Title "Introduction page." ()
+  Body $ do
+    mult Div (id "header") $ Content "Syntax" ()
+    P $ Content "This is an example of Blazemarkup syntax." ()
+    UL $ foldHtml $ (map (LI . toMarkup . show) [1, 2, 3])
 
 main :: IO ()
-main = putStrLn "Test"
--- main = do
---   putStrLn $ renderString "" $ Link ! rel "style.css" ! type_ "text/html"
---   putStrLn $ renderString "" $ Head (Append (Append (Link ! rel "style.css" ! type_ "text/html") (HRef "LinkName" ! href "http://blaze-sample.com")) (Content "content"))
+main = do
+  putStrLn $ renderMarkup "" page1
+
+  -- putStrLn $ renderMarkup "" $ Head
+  --   (Append
+  --     (Append (Link ! rel "style.css" ! type_ "text/html") (HRef "LinkName" ! href "http://blaze-sample.com")) (Content "content"))
